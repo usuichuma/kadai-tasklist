20ad260f7da830d5895cfb4baea01a66df964548
usui.chuma <usuichuma@icloud.com>
2021-10-23 12:56:53 +0000

    13

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 before_action :require_user_logged_in
-before_action :correct_user, only: [:edit, :destroy, :update]
+before_action :correct_user, only: [:shoe, :edit, :destroy, :update]
 before_action :set_task, only: [:show, :edit, :update, :destroy]
 
 
